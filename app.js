19e9d24b368d99aae94a47a1eb96c99dfea6a101
SergeyYepik <sergey.a.epik@gmail.com>
2022-07-02 18:43:01 +0300

    ver 2.0.4

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 function full_nav(){
     document.getElementsByClassName('sidenav')[0].style.width = '100%'
+    document.getElementsByClassName('sidenav')[0].style.paddinTop = '200px'
+    document.getElementsByClassName('sidenav')[0].style.overflowY = 'scroll'
+
+    document.getElementsByClassName('whiteownerName')[0].style.visibility = 'visible'
+    document.getElementsByClassName('whiteownerName')[0].style.top = '270px'
+    document.getElementsByClassName('contacts')[0].style.top = '300px'
+    document.getElementsByClassName('education')[0].style.top = '550px'
+    document.getElementsByClassName('personal')[0].style.top = '660px'
+
+  
+
+
 }
 
 function full_main(){
