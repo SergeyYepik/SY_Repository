4f994c29947ac403ad0353d62f945e40edfa6f8f
SergeyYepik <sergey.a.epik@gmail.com>
2022-07-03 12:44:27 +0300

    ver. 2.1.0

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
 function full_nav(){
-    document.getElementsByClassName('sidenav')[0].style.width = '100%'
-    document.getElementsByClassName('sidenav')[0].style.paddinTop = '200px'
-    document.getElementsByClassName('sidenav')[0].style.overflowY = 'scroll'
+    if (document.getElementsByClassName('button_right')[0].innerHTML == '<img src="./images/arrow_forward.svg">')
+    {
+        document.getElementsByClassName('sidenav')[0].style.width = '100%'
+        document.getElementsByClassName('sidenav')[0].style.paddinTop = '200px'
+        document.getElementsByClassName('sidenav')[0].style.overflowY = 'scroll'
 
-    document.getElementsByClassName('whiteownerName')[0].style.visibility = 'visible'
-    document.getElementsByClassName('whiteownerName')[0].style.top = '270px'
-    document.getElementsByClassName('contacts')[0].style.top = '300px'
-    document.getElementsByClassName('education')[0].style.top = '550px'
-    document.getElementsByClassName('personal')[0].style.top = '660px'
+        document.getElementsByClassName('whiteownerName')[0].style.visibility = 'visible'
+        document.getElementsByClassName('contacts')[0].style.top = '300px'
+        document.getElementsByClassName('education')[0].style.top = '550px'
+        document.getElementsByClassName('personal')[0].style.top = '660px'
 
-  
+        document.getElementsByClassName('button_right')[0].innerHTML = '<img src="./images/arrow_back.svg">'
+    } else 
+        {
+            document.getElementsByClassName('sidenav')[0].style.width = '250px'
+            document.getElementsByClassName('sidenav')[0].style.paddinTop = '0px'
+            document.getElementsByClassName('sidenav')[0].style.overflowY = 'auto'
 
+            document.getElementsByClassName('whiteownerName')[0].style.visibility = 'hidden'
+            document.getElementsByClassName('contacts')[0].style.top = '250px'
+            document.getElementsByClassName('education')[0].style.top = '500px'
+            document.getElementsByClassName('personal')[0].style.top = '610px'
 
+            document.getElementsByClassName('button_right')[0].innerHTML = '<img src="./images/arrow_forward.svg">'
+        }
 }
 
 function full_main(){
-    document.getElementsByClassName('sidenav')[0].style.width = '0px'
-    document.getElementsByClassName('sidenav')[0].style.height = '0px'
+    if (document.getElementsByClassName('button_left')[0].innerHTML == '<img src="./images/arrow_back.svg">')
+    {
+        document.getElementsByClassName('sidenav')[0].style.width = '0px'
+        document.getElementsByClassName('sidenav')[0].style.height = '0px'  
+        document.getElementsByClassName('sidenav')[0].style.position = 'relative'
 
-    document.getElementsByClassName('sidenav')[0].style.position = 'relative'
-    document.getElementsByClassName('main')[0].style.margin = '0px'
-    document.getElementsByClassName('main')[0].style.top = '0px'
-    document.getElementsByClassName('main')[0].style.position = 'relative'
+        document.getElementsByClassName('main')[0].style.left = '0px'
+        document.getElementsByClassName('main')[0].style.width = '100%'
+        
+        document.getElementsByClassName('button_left')[0].innerHTML = '<img src="./images/arrow_forward.svg">'
+    } else{
+        document.getElementsByClassName('sidenav')[0].style.width = '250px'
+        document.getElementsByClassName('sidenav')[0].style.height = '100%'
+        document.getElementsByClassName('sidenav')[0].style.position = 'fixed'
+
+        document.getElementsByClassName('main')[0].style.left = '250px'
+        document.getElementsByClassName('main')[0].style.width = 'calc(100% - 250px)'
+
+        document.getElementsByClassName('button_left')[0].innerHTML = '<img src="./images/arrow_back.svg">'
+    }
 }
 
 
